8b9e0d6d903e4a5635853bc003e4bca322ad704e
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-08-04 23:50:23 +0530

    Add images and metadata to `src/assets/` folder and upload to pinata

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Images: https://gateway.pinata.cloud/ipfs/QmXZ3TgRgd5EZEk2DhwGvjf8f6sQJNCrnHzrEw1oHufgnL/
+// Metdata: https://gateway.pinata.cloud/ipfs/QmXFVudHxeRx4FMACDXoN8YGE5KnPvbe5DEpehV4ssid29/
